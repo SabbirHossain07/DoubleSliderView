12e04b25be8281693ca917b5fadd5387ba035be0
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-11 20:54:55 +0600

    Initial Commit

---
+++ DoubleSliderView/DoubleSliderViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoubleSliderViewApp.swift
+//  DoubleSliderView
+//
+//  Created by Sopnil Sohan on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DoubleSliderViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
